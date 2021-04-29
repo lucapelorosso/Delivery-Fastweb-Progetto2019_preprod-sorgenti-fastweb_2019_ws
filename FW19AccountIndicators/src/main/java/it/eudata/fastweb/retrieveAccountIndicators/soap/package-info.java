1488ab82a796049aa23a15f0817b1bef1abd9500
Luca Pelorosso <lpelo2000@gmail.com>
2021-04-29 15:30:10 +0200

    Delete FW19AccountIndicators directory

--- FW19AccountIndicators/src/main/java/it/eudata/fastweb/retrieveAccountIndicators/soap/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://it.fastweb.esb.org/RetrieveAccountIndicators", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package it.eudata.fastweb.retrieveAccountIndicators.soap;
