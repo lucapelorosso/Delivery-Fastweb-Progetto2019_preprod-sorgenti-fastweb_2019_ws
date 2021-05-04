cd46a664f5e4def9b5f647f2b2eea06ee50ffa5a
Luca Pelorosso <lpelo2000@gmail.com>
2021-05-04 14:52:05 +0200

    aa
    aa

--- FW_Services/scripts/start_tomcat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-path_base='/opt/tomcat/webapps'
-
-echo -e "\nStart apache ..."
-sudo rm -rf $path_base/work/*
-sudo chown tomcat:tomcat -R $path_base
-sudo systemctl start tomcat
-sudo systemctl restart awslogsd
-echo -e "\ncompleted\n"
--- FW_Services/scripts/stop_tomcat.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo -e "\nStop apache ..."
-sudo systemctl stop tomcat
-echo -e "\ncompleted\n"
